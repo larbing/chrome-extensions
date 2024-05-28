3c12038b643abec0cb337a4fdd6a0d5a0a09a2e5
larbing <larbing@163.com>
2024-05-28 15:15:18 +0800

    up

--- douban/contentScript.js
+++ douban/contentScript.js
@@ -1,14 +1,38 @@
+function createAndInsertLink(spanElement) {
+    const newLink = document.createElement('a');
+    newLink.href = 'https://so.sonainai.com/search?q=' + spanElement.textContent.split(" ")[0];
+    newLink.target = '_blank';
+    newLink.textContent = spanElement.textContent.trim();
+
+    spanElement.textContent = ""; 
+    spanElement.appendChild(newLink); 
+}
+
+function updateListWithHtml(name, targetElement) {
+    fetch("https://so.sonainai.com/player_links?name="+name)
+      .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP request failed with status: ${response.status}`);
+            }
+            return response.text();
+        })
+      .then(html => {
+            targetElement.innerHTML = html;
+        })
+      .catch(error => {
+            console.error('Failed to fetch and update list:', error);
+        });
+}
+
+// 主逻辑
 const spanElement = document.querySelector('span[property="v:itemreviewed"]');
 if (spanElement) {
-    const newLink = document.createElement('a');
-    newLink.href = 'https://so.sonainai.com/search?q='+spanElement.textContent.split(" ")[0];
-    newLink.target = '_blank'; 
-    newLink.textContent = spanElement.textContent; 
-  
-    spanElement.textContent = "";
-    spanElement.appendChild(newLink);
-
-
-} else {
-    console.log("null");
+    createAndInsertLink(spanElement);
+    const name = spanElement.textContent.split(" ")[0];
+    const ulElement = document.querySelector('ul.bs');
+    if (ulElement) {
+        updateListWithHtml(name,ulElement);
+    } else {
+        console.log('No matching element found.');
+    }
 }
